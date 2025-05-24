2b7322ff3d4cadd6c98630ba921b8fad1495da0c
Khoi Duong <khoiduongbusiness@gmail.com>
2025-05-24 19:29:03 -0400

    movement is done, need to add draw board then GUI

--- 2048/utility.c
+++ 2048/utility.c
@@ -1,7 +1,9 @@
 // CODE: Inlcude necessary librar(y/ies)
 #include <termios.h> // used in getch_unix()
 #include <unistd.h>  // used in getch_unix()
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "utility.h"
 
 // CODE: the implementation of functions and related variables 
 
@@ -30,3 +32,191 @@ char getch_unix() {
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     return ch;
 }
+
+int move_left(int board[size][size]){
+    int moved = 0; // This is used as a boolean to show tell the program whether the player has inputted a move command or not
+    for (int i = 0; i < size; i++){
+        int temp[size];
+        int result[size];
+        int x = 0;
+        for (int j = 0; j < size; j++){
+            if (board[i][j] != 0){
+                temp[x] = board[i][j]; // Assign the value at of the board onto the temp array at index x before incrementing
+                x++;
+            }
+        }
+
+        for (int j = x; j < size; j++){
+            temp[j] = 0;
+        }
+
+        // This loop here is to merge all of the tiles with the same number together and doubling them
+        for (int j = 0; j < size-1; j++){
+            if (temp[j] != 0 && temp[j] == temp[j+1]){
+                temp[j] *= 2;
+                temp[j+1] = 0;
+                moved = 1;
+                j++;
+            }
+        }
+        int y = 0;
+        for (int j = 0; j < size; j++){
+            if (temp[j] != 0){
+                result[y] = temp[j];
+                y++;
+            }
+        }
+        for (int j = y; j < size; j++){
+            result[j] = 0;
+        }
+
+        for (int j = 0; j < size; j++){
+            if (board[i][j] != result[j]){
+                moved = 1;
+            }
+            board[i][j] = result[j];
+        }
+    }
+
+    return moved;
+}
+
+int move_right(int board[size][size]){
+    int moved = 0;
+    for (int i = 0; i < size; i++){
+        int temp[size];
+        int result[size];
+        int x = 0;
+        for (int j = size - 1; j >= 0; j--){
+            if(board[i][j] != 0){
+                temp[x] = board[i][j];
+                x++;
+            }
+        }
+        for (int j = x; j < size; j++){
+            temp[j] = 0;
+        }
+
+        for (int j = 0; j < size - 1; j++){
+            if(temp[j] != 0 && temp[j] == temp[j+1]){
+                temp[j] *= 2;
+                temp[j + 1] = 0;
+                moved = 1;
+                j++;
+            }
+        }
+        int y = 0;
+        for (int j = 0; j < size; j++){
+            if(temp[j] != 0){
+                result[y] = temp[j];
+                y++;
+            }
+        }
+
+        for (int j = y; j < size; j++){
+            result[j] = 0;
+        }
+
+        for (int j = 0; j < size; j++){
+            if (board[i][size-j-1] != result[j]){
+                moved = 1;
+            }
+            board[i][size-j-1] = result[j];
+        }
+    }
+
+    return moved;
+}
+
+int move_up(int board[size][size]){
+    int moved = 0;
+    for (int j = 0; j < size; j++){
+        int temp[size];
+        int result[size];
+        int x = 0;
+        for (int i = 0; i < size; i++){
+            if (board[i][j] != 0){
+                temp[x] = board[i][j];
+                x++; 
+            }
+        }
+        for (int i = x; i < size; i++){
+            temp[i] = 0;
+        }
+
+        for (int i = 0; i < size - 1; i++){
+            if(temp[i] != 0 && temp[i] == temp[i+1]){
+                temp[i] *= 2;
+                temp[i+1] = 0;
+                moved = 1;
+                i++;
+            }
+        }
+        int y = 0;
+        for (int i = 0; i < size; i++){
+            if (temp[i] != 0){
+                result[y] = temp[i];
+                y++;
+            }
+        }
+        for (int i = y; i < size; i++){
+            result[i] = 0;
+        }
+        for (int i = 0; i < size; i++){
+            if(board[i][j] != result[i]){
+                moved = 1;
+            }
+            board[i][j] = result[i];
+        }
+    }
+    return moved;
+}
+
+int move_Down(int board[size][size]){
+    int moved = 0;
+    for (int j = 0; j < size; j++){
+        int temp[size];
+        int result[size];
+        int x = 0;
+        for (int i = size - 1; i >= 0; i--){
+            if (board[i][j] != 0){
+                temp[x] = board[i][j];
+                x++;
+            }
+        }
+
+        for (int i = x; i < size; i++){
+            temp[i] = 0;
+        }
+
+        for (int i = 0; i < size - 1; i++){
+            if (temp[i] != 0 && temp[i] == temp[i+1]){
+                temp[i] *= 2;
+                temp[i + 1] = 0;
+                moved = 1;
+                i++;
+            }
+        }
+
+        int y = 0;
+        for (int i = 0; i < size; i++){
+            if (temp[i] != 0){
+                result[y] = temp[i];
+                y++;
+            }
+        }
+        for (int i = y; i < size; i++){
+            result[i] = 0;
+        }
+
+        for (int i = 0; i < size; i++){
+            if (board[size-i-1][j] != result[i]){
+                moved = 1;
+            }
+            board[size-i-1][j] = result[i];
+        }
+    }
+
+    return moved;
+}
+
